9462fc8d9374605b1b2a0d87d6de94a88f5022f8
Rachel Tao <taorachel@microsoft.com>
2025-02-05 18:47:01 -0500

    updating default environment to preship

--- utils/arguments.py
+++ utils/arguments.py
@@ -4,7 +4,7 @@ import argparse
 def get_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--env", "-e", help="Environment", default="glab")
+    parser.add_argument("--env", "-e", help="Environment", default="preship")
     parser.add_argument("--log-level", "-l", help="Log level", default="INFO")
 
     return parser.parse_args()
